a756bc05dd26194d3290da57f20994ed9530a28c
thararatSamniang <thararatkub@gmail.com>
2023-10-19 23:35:11 +0700

    pimm

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import useViewModel from '@Component/Calendar/useViewModel';
+import useViewModel from './useViewModel';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCaretRight, faCaretLeft} from '@fortawesome/free-solid-svg-icons';
