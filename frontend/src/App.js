26a2db3e245dc767372eb47e28bcc111db1c7c25
songkun <songkunudom@gmail.com>
2023-10-21 11:04:19 +0700

    Pastfood
    Pastfood

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 
 // import Login from "./components/Login";
-import MyFood from "./components/MyFood";
+// import MyFood from "./components/MyFood";
 // import Addfood from "./components/Addfood";
 // import Cookfood from "./components/Cookfood";
 // import Buyfood from "./components/Buyfood";
+import Pastfood from "./components/Pastfood";
+// import PieColor from "./components/Piedayly";
 function App() {
   return (
     <>
-      <MyFood />
+      <Pastfood />
     </>
   );
 }
---
+++ frontend/src/components/Pastfood.js
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+import PieColor from "./Piedayly";
+
+const Pastfood = ({ className }) => (
+  <div className={className}>
+    <div className="back">
+      <button class="btn btn-active btn-secondary">⬅back</button>
+    </div>
+    <div className="Text">
+      <h1>
+        <b>ข้าวสวยหอมมะลิตราอีซี่โก</b>
+      </h1>
+    </div>
+    <div className="Nutritions">
+      <h2>
+        <b>Nutritions</b>
+        </h2>
+      <h1>
+        <b>สารอาหาร</b>
+      </h1>
+    </div>
+    <div className="num">
+
+    <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text1">จำนวน</span>
+            <span className="label-text-alt">หน่วยตามที่บรรจุ</span>
+          </label>
+          <input
+           type="number"
+           min="1"
+            placeholder="1"
+            className="input input-bordered input-error w-full max-w-xs"
+          />
+          <br/>
+          <PieColor />
+        </div>
+    </div>
+   
+
+
+  </div>
+  
+);
+
+export default styled(Pastfood)`
+  .back {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    padding: 10px 20px;
+    font-size: calc(60% + 2vmin);
+  }
+  .Text h1 {
+    font-size: calc(60% + 2vmin);
+    color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5rem;
+  }
+  .Nutritions h2{
+    font-size: calc(60% + 1vmin);
+    color: #2baf2b;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+  .Nutritions h1{
+    font-size: calc(60% + 2vmin);
+    color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .num {
+    font-size: calc(60% + 2vmin);
+    padding: 50px;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
