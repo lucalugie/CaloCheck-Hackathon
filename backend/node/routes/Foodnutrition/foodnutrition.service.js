16834d7a61a1aa239180391219662b4b89a0ddb7
songkun <songkunudom@gmail.com>
2023-10-27 19:21:48 +0700

    แดง

--- backend/node/routes/Foodnutrition/foodnutrition.service.js
+++ backend/node/routes/Foodnutrition/foodnutrition.service.js
@@ -1,5 +1,6 @@
 const Foodnutrition = require('../../model/foodnutrition');
 const { Op } = require("sequelize");
+
 async function sendFoodnu(req, res) {
     const foods = await Foodnutrition.findAll();
     res.send(foods);
@@ -33,46 +34,27 @@ async function createFoodnu (req, res){
 }
 
 async function searchmenu(req, res) {
-try{
-const nameToSearch = req.query.name;
-  const food = await Foodnutrition.findAll({
-    where: {
-      name: {
-        [Op.like]: `%${nameToSearch}%`
+  try{
+  const nameToSearch = req.query.name;
+    const food = await Foodnutrition.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${nameToSearch}%`
+        }
       }
+    });
+    if (food) {
+      res.status(200).json(food); 
+      
+    } else {
+      res.status(404).json({ message: "food not found" });
     }
-  });
-  if (food) {
-    res.status(200).json(food); 
-  } else {
-    res.status(404).json({ message: "food not found" });
   }
-}
-catch (error) {
-  console.error(error);
-}
-}
+  catch (error) {
+    console.error(error);
+  }
+  }
 
-async function searchmenu(req, res) {
-try{
-const nameToSearch = req.query.name;
-  const food = await Foodnutrition.findAll({
-    where: {
-      name: {
-        [Op.like]: `%${nameToSearch}%`
-      }
-    }
-  });
-  if (food) {
-    res.status(200).json(food); 
-  } else {
-    res.status(404).json({ message: "food not found" });
-  }
-}
-catch (error) {
-  console.error(error);
-}
-}
 
 module.exports = {
     sendFoodnu,
