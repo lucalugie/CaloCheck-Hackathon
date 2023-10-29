b41abeecd6bec165091a51ee511c70485008b222
lucalugie <118959975+lucalugie@users.noreply.github.com>
2023-10-30 00:08:32 +0700

    User setting +update info/goals Done

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
-import userSlice from "./userSlice"
-import aiPageSlice from './aiPageSlice';
-import setSearch from './setSearch';
+import userSlice from "./userSlice";
+import aiPageSlice from "./aiPageSlice";
+import setSearch from "./setSearch";
+import userGoalSlice from "./userGoalSlice";
 
 export default configureStore({
-    reducer:{
-        user: userSlice,
-        aiPage: aiPageSlice,
-        search:setSearch
-    }
+  reducer: {
+    user: userSlice,
+    aiPage: aiPageSlice,
+    search: setSearch,
+    goals: userGoalSlice,
+  },
 });
