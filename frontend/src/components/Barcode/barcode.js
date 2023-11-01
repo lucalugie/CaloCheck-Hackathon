31434ecf2acfd2c43a979c155f39330c0e717313
lucalugie <118959975+lucalugie@users.noreply.github.com>
2023-11-01 17:02:22 +0700

    bacode

---
+++ frontend/src/components/Barcode/barcode.js
@@ -0,0 +1,45 @@
+import Scanner from './Scanner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarcode } from '@fortawesome/free-solid-svg-icons';
+import BarcodeFunctions from './barcodeFunctions';
+import { Preview } from '@mui/icons-material';
+const Barcode = () => {
+    const {scannerRef, cameraId, setCameraId, scanning, setScanning, setResults, cameraError, cameras} = BarcodeFunctions();
+  return (
+    <>
+        <button className="btn btn-accent ml-2" onClick={()=>document.getElementById('my_modal_2').showModal()}>
+              <FontAwesomeIcon icon={faBarcode} className="input-icon" />
+        </button>
+        <dialog id="my_modal_2" className="modal">
+            <div className="modal-box w-11/12 max-w-5xl h-auto flex justify-center items-center">
+                <div className='flex-1'>
+                    {cameraError ? <p>ERROR INITIALIZING CAMERA -- DO YOU HAVE PERMISSION?</p> : null}
+                    {cameras.length === 0 ? <p>Enumerating Cameras, browser may be prompting for permissions beforehand</p> :
+                        <form>
+                            <select onChange={(event) => setCameraId(event.target.value)}>
+                                {cameras.map((camera) => (
+                                    <option key={camera.deviceId} value={camera.deviceId}>
+                                        {camera.label || camera.deviceId}
+                                    </option>
+                                ))}
+                            </select>
+                        </form>
+                    }
+                    <button className='btn' onClick={() => setScanning(!scanning) }>{scanning ? 'หยุด' : 'เริ่ม'}</button>
+                    <div ref={scannerRef} className="w-auto min-h-[75vh]  md:min-h-[50vh]">
+                        {scannerRef.current && <canvas className="drawingBuffer absolute" width={scannerRef.current.offsetWidth} height={scannerRef.current.offsetHeight}></canvas>}
+                        {scanning ? <Scanner scannerRef={scannerRef} cameraId={cameraId} onDetected={(result) => setResults(result)}  constraints={{width: scannerRef.current.offsetWidth, height: scannerRef.current.offsetHeight}}/> : null}
+                    </div>
+                </div>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+
+                
+    </>
+  );
+};
+
+export default Barcode;
