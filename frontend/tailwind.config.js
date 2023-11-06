4bff2eec20eacbe6ba2c0724014b633411a5b858
Yungyingzerza <yungyingzerza2547@gmail.com>
2023-11-06 07:19:23 +0700

    3

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        test: "#e96d7b",
+        Glossy1: "rgba(255, 255, 255, 0) 0%",
+        Glossy2: "rgba(255, 255, 255, 0) 35%",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+};
